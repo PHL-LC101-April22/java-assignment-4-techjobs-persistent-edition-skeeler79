db3e6d7d324a851a3ce09f68d1525bfcdc6b8607
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-09-30 15:23:26 -0400

    finish part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- Part 1: Test it with SQL
-
+--int id, String employer, String name, String skills
 -- Part 2: Test it with SQL
+--SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +15,15 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+@Autowired
+    EmployerRepository employerRepository;
+
+    @GetMapping("")
+    public String index(Model model){
+        model.addAttribute("title", "All Employers");
+        model.addAttribute("employer", employerRepository.findAll());
+        return "index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -25,16 +36,17 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
             return "employers/add";
         }
-
-        return "redirect:";
+        employerRepository.save(newEmployer);
+        return "employers/view";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
